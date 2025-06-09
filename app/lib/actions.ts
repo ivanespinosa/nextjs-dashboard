132accc306349de284fd0407c207be35aa44e1d9
Ivan Espinosa <ivan.espinosaf@gmail.com>
2025-06-08 22:22:53 -0600

    Validar en prod el revalidatePath (descomentado) de dashboard/invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -31,6 +31,6 @@ export async function createInvoice(formData: FormData) {
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
 
-  // revalidatePath("/dashboard/invoices");
+  revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
